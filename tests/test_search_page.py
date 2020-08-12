743770b01e54b4b533822ac4b5dec66d31661662
Sergii Rubanenko <sergii.rubanenko@gmail.com>
2020-08-12 19:07:05 +0300

    Initial commit

---
+++ tests/test_search_page.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TestSearchPage:
+
+    @pytest.mark.smoke
+    def test_search_1(self):
+        driver = webdriver.Chrome('./chromedriver')
+        session_id = driver.session_id
+        link = "https://www.python.org"
+        driver.get(link)
+        print(driver.title)
+        try:
+            search_bar = driver.find_element_by_name("q")
+            search_bar.clear()
+            search_word = 'python'
+            search_bar.send_keys(search_word)
+            search_bar.send_keys(Keys.RETURN)
+            url= driver.current_url
+            assert link + '/search/?q=' + search_word in url
+        except AssertionError:
+            driver.get_screenshot_as_file(session_id+'.png')
+            raise
+        finally:
+            driver.close()
+
+    @pytest.mark.smoke
+    def test_search_2(self):
+        driver = webdriver.Chrome('./chromedriver')
+        session_id = driver.session_id
+        driver.get("https://www.python.org")
+        try:
+            title = driver.title
+            print(title)
+            assert 'java' in title
+        except AssertionError:
+            driver.get_screenshot_as_file(session_id+'.png')
+            raise
+        finally:
+            driver.close()
+
